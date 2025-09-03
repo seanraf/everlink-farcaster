9cc0ee76e6ce8690936225b21173fbe9e0e9d501
zawar.shafiq@mavrictech.com <zawar.shafiq@mavrictech.com>
2025-09-03 03:15:33 -0700

    Add console for status of variables

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,6 +2,7 @@ import { http, createConfig } from 'wagmi';
 import { base, mainnet, optimism } from 'wagmi/chains';
 import { injected, metaMask, safe, walletConnect } from 'wagmi/connectors';
 
+console.log({ base, mainnet, optimism });
 const projectId = '3b0fe6df709f252749123eed4fbfeba4';
 
 export const config = createConfig({
