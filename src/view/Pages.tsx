fe659aa4d528fbdeb171ac9d90573b6a5b08f6d4
zawar.shafiq@mavrictech.com <zawar.shafiq@mavrictech.com>
2025-11-06 16:46:13 +0500

    Minting Contract add

--- src/view/Pages.tsx
+++ src/view/Pages.tsx
@@ -4,8 +4,9 @@ import { useAuth } from '@crossmint/client-sdk-react-ui';
 
 import Loader from './Loader';
 import { useFrameContext } from '../providers/FarcasterContextProvider';
-import MintWithMetamask from '../components/MintWithMetamask';
+// import MintWithMetamask from '../components/MintWithMetamask';
 import EverlinkPages from './EverlinkPages';
+import MintingContract from '../components/MintingContract';
 // import EverlinkPages from './EverlinkPages';
 
 export default function Pages({
@@ -37,7 +38,8 @@ export default function Pages({
   return isUserAuthenticated ? (
     <>
       <EverlinkPages />
-      <MintWithMetamask />
+      {/* <MintWithMetamask /> */}
+      <MintingContract />
     </>
   ) : (
     <LandingPage />
