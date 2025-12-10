e48d33a8bc1470636147430b3590d11b356819ec
zawar.shafiq@mavrictech.com <zawar.shafiq@mavrictech.com>
2025-12-10 18:04:29 +0500

    Update component

--- src/components/MintThroughFarcaster.tsx
+++ src/components/MintThroughFarcaster.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { createPublicClient, http } from 'viem';
 import { base } from 'viem/chains';
 import { useAccount, useConnect, useReconnect } from 'wagmi';
